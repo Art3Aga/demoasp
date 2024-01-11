1f7dd0ac3fb440a7f562c72f5141b02932a1517a
dealvarado <dealvarado@gbm.net>
2024-01-11 14:27:23 -0600

    tamaño

--- obj/Release/net6.0/TodoList.AssemblyInfo.cs
+++ obj/Release/net6.0/TodoList.AssemblyInfo.cs
@@ -11,8 +11,13 @@
 using System;
 using System.Reflection;
 
-[assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.ProvideApplicationPartFactoryAttribute("Microsoft.AspNetCore.Mvc.ApplicationParts.ConsolidatedAssemblyApplicationPartFact" +
-    "ory, Microsoft.AspNetCore.Mvc.Razor")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("TodoList")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyProductAttribute("TodoList")]
+[assembly: System.Reflection.AssemblyTitleAttribute("TodoList")]
+[assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.
 
